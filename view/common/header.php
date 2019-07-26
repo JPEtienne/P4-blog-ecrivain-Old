f2108818521e3d3f47a49d572bb4dfe981e42ba0
Jester0718 <ji.pe.7@hotmail.com>
2019-07-26 03:15:53 +0200

    Adjusting informations

--- view/common/header.php
+++ view/common/header.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Le Livre Blanc</title>
+    <title>Billet simple pour l'Alaska</title>
     <link rel="icon" type="image/ico" href="public/images/favicon.ico"
-    <meta name="description" content="Blog de nouvelles sur la littérature moderne">
+    <meta name="description" content="Blog de publication en ligne du romain Billet simple pour l'Alaska">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="public/css/main.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -18,7 +18,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container">
-  <a class="navbar-brand" href="home-1">Le Livre Blanc</a>
+  <a class="navbar-brand" href="home-1">Billet simple pour l'Alaska</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
